d5a2416061cc3bc83145e9e7fa1840822e161b9e
Jonny Gates <jonny.gates@outlook.com>
2019-02-25 22:32:35 +0000

    Add create action to lists

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,24 @@
 class ListsController < ApplicationController
+
   def new
     @list = List.new
   end
+
+  def create
+    @list = List.new(list_params)
+    @list.user = current_user
+
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+
 end
