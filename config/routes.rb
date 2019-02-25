13e4be96ad3bd014a38655c3eb3536642bd93368
Jonny Gates <jonny.gates@outlook.com>
2019-02-25 22:28:09 +0000

    Create add and index on the homepage

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
+
   resources :lists do
     resources :items, only: [:new, :create]
   end
+
 
   resources :items
 end
